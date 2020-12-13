955271bb802beaef8aeb23ae1ef696e47c37ccf0
Ngo <nghiangominh99@gmail.com>
2020-12-13 20:32:00 +0700

    Init UI
    Tạo tất cả các UI cần thiết bào gồm:
    - Main (hiển thị giao diện bảng dữ liệu)
    - Component Login
    - Arlet: thông báo thành công
    - Dialog: Xác nhận xóa dữ liệu
    Các component được tạo dựa trên JavaFX 11.0.1

---
+++ src/ui/main/Main.java
@@ -0,0 +1,26 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("CRUI Framework version 1.0");
+        primaryStage.centerOnScreen();
+        Scene newScene = new Scene(root, 1280  , 720);
+        primaryStage.setScene(newScene);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
