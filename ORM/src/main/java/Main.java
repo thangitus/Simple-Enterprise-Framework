6ef6550b23ed49b663d324a656da90b1ded13965
PhatFood <baophatnguyen99@gmail.com>
2020-12-19 00:25:20 +0700

    test Hibernate

--- ORM/src/main/java/Main.java
+++ ORM/src/main/java/Main.java
@@ -6,41 +6,6 @@ import java.util.List;
 
 class Main {
    public static void main(String args[]) {
-      try {
-         Class.forName("com.mysql.cj.jdbc.Driver");
-         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hibernateexercise", "root",
-                                                             "1111");
-
-         DatabaseMetaData metadata = connection.getMetaData();
-         ResultSet resultSet = metadata.getTables(connection.getCatalog(), null, "%", null);
-
-         while (resultSet.next()) {
-            System.out.println(resultSet.getString("TABLE_NAME"));
-         }
-         // Create a result set
-
-         Statement statement = connection.createStatement();
-
-         ResultSet results = statement.executeQuery("SELECT * FROM chi_tiet_lop_hoc");
-
-         // Get resultset metadata
-
-         ResultSetMetaData resultSetMetaData = results.getMetaData();
-
-         int columnCount = resultSetMetaData.getColumnCount();
-
-         System.out.println("chi_tiet_lop_hoc columns : ");
-
-         // Get the column names; column indices start from 1
-         for (int i = 1; i <= columnCount; i++) {
-            String columnName = resultSetMetaData.getColumnName(i);
-            System.out.println(resultSetMetaData.getColumnType(i));
-            System.out.println(columnName);
-         }
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-
       // - Test hibernate ------
       ChiTietLopHocDAO chiTietLopHocDAO = new ChiTietLopHocDAO();
       chiTietLopHocDAO.save(new ChiTietLopHoc("aaa","bbb","ccc"));
