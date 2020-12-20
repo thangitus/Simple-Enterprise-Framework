2958b8c926874af303c0065c60af40955615889a
PhatFood <baophatnguyen99@gmail.com>
2020-12-21 02:40:12 +0700

    persistence gen test

--- src/main/java/orm/Main.java
+++ src/main/java/orm/Main.java
@@ -1,16 +1,25 @@
 package orm;
 
+import orm.config.PersistenceConfig;
+
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 class Main {
    public static void main(String args[]) {
-      SqlServer sqlServer = new SqlServer("root", "admin");
+      SqlServer sqlServer = new SqlServer("root", "1111");
       List<String> databases = sqlServer.connectToServer();
-      SqlDatabase sqlDatabase = sqlServer.connectToDatabase("hanks");
+      SqlDatabase sqlDatabase = sqlServer.connectToDatabase("hibernateexercise");
       File file = new File("E:\\Nam Four\\tmp");
       if (sqlDatabase != null) {
          sqlDatabase.generate(file);
       }
+
+      List<String> entityClasses = new ArrayList<>();
+      entityClasses.add("entity.SinhVien");
+      entityClasses.add("entity.LopHoc");
+      PersistenceConfig persistenceConfig = new PersistenceConfig(entityClasses, "root","1111","com.mysql.jdbc.Driver","jdbc:mysql://localhost:3306/hibernateexercise");
+      persistenceConfig.generate(new File("E:\\Nam Four\\tmp\\persistence.xml"));
    }
-}  
+}
---
+++ src/main/java/orm/config/PersistenceConfig.java
@@ -0,0 +1,102 @@
+package orm.config;
+
+import generator.Generatable;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PersistenceConfig implements Generatable {
+    private List<String> entityClasses;
+    private String user;
+    private String passWord;
+    private String jdbcDriver;
+    private String url;
+
+    public PersistenceConfig(){
+
+    }
+
+    public PersistenceConfig(List<String> entityClasses, String user, String passWord, String jdbcDriver, String url) {
+        this.entityClasses = entityClasses;
+        this.user = user;
+        this.passWord = passWord;
+        this.jdbcDriver = jdbcDriver;
+        this.url = url;
+    }
+
+    @Override
+    public void generate(File directory) {
+        Path path = Paths.get("src\\main\\java\\orm\\config\\PersistenceTemplate.txt");
+        StringBuilder builder = new StringBuilder("");
+
+        // Read persistence template file
+        try {
+            Files.lines(path).forEach((line)->{builder.append(line+"\n");});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Convert entity classes name to right format
+        String entities = entityClasses
+                .stream()
+                .map(classEntity->"<class>" + classEntity + "</class>")
+                .reduce("",(a,b)-> a + b);
+
+        String finalPersistenceContent = builder.toString();
+
+        finalPersistenceContent = StringUtils.replace(finalPersistenceContent, "%entity%", entities);
+        finalPersistenceContent = StringUtils.replace(finalPersistenceContent, "%jdbcDriver%", jdbcDriver);
+        finalPersistenceContent = StringUtils.replace(finalPersistenceContent, "%user%", user);
+        finalPersistenceContent = StringUtils.replace(finalPersistenceContent, "%passWord%", passWord);
+        finalPersistenceContent = StringUtils.replace(finalPersistenceContent, "%url%", url);
+
+        try {
+            if (directory.createNewFile()){
+                FileWriter myWriter = new FileWriter(directory);
+                myWriter.write(finalPersistenceContent);
+                myWriter.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<String> getEntityClasses() {
+        return entityClasses;
+    }
+
+    public void setEntityClasses(List<String> entityClasses) {
+        this.entityClasses = entityClasses;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getJdbcDriver() {
+        return jdbcDriver;
+    }
+
+    public void setJdbcDriver(String jdbcDriver) {
+        this.jdbcDriver = jdbcDriver;
+    }
+}
