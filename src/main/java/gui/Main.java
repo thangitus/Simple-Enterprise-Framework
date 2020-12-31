6bdecaf33598cfdf713de6c701479b364ebea9ef
Nguyen Tuan Dat <53074785+itdat@users.noreply.github.com>
2021-01-01 02:35:05 +0700

    Init project

---
+++ src/main/java/gui/Main.java
@@ -0,0 +1,23 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Main.fxml"));
+        primaryStage.setTitle("Database Connection");
+        primaryStage.setScene(new Scene(root, 900, 600));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
