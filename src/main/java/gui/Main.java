d0456ad8215458bc82f973b1ec2f5ae41049d0c1
Nguyen Tuan Dat <53074785+itdat@users.noreply.github.com>
2021-01-01 03:57:35 +0700

    Add basic layout for Login screen with Material UI

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -1,6 +1,10 @@
 package gui;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -9,11 +13,24 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+    @FXML
+    private JFXTextField edtUsername;
+
+    @FXML
+    private JFXPasswordField edtPassword;
+
+    @FXML
+    private JFXTextArea edtDatabaseUri;
+
+
     @Override
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Main.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/css/Main.css").toExternalForm());
         primaryStage.setTitle("Database Connection");
-        primaryStage.setScene(new Scene(root, 900, 600));
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
