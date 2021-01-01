e550eafa549401445249863c05a9d1b1a3bafba4
Nguyen Tuan Dat <53074785+itdat@users.noreply.github.com>
2021-01-02 02:31:32 +0700

    Init layout for ConfigScreen

--- src/main/java/gui/LoginScreen.java
+++ src/main/java/gui/LoginScreen.java
@@ -2,8 +2,6 @@ package gui;
 
 import com.jfoenix.controls.*;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -16,7 +14,7 @@ import orm.SqlServer;
 import java.io.IOException;
 import java.util.List;
 
-public class Main extends Application {
+public class LoginScreen extends Application {
     @FXML
     private StackPane root;
 
@@ -34,7 +32,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/LoginScreen.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/css/Main.css").toExternalForm());
         primaryStage.setTitle("Database Connection");
