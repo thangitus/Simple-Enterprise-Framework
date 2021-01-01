451f150ba6201297e40923aac4d7295f18430ec3
Nguyen Tuan Dat <53074785+itdat@users.noreply.github.com>
2021-01-01 11:34:39 +0700

    Improve UI and add dialog

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -1,18 +1,23 @@
 package gui;
 
-import com.jfoenix.controls.JFXPasswordField;
-import com.jfoenix.controls.JFXTextArea;
-import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.*;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
+    @FXML
+    private StackPane root;
+
     @FXML
     private JFXTextField edtUsername;
 
@@ -21,6 +26,9 @@ public class Main extends Application {
 
     @FXML
     private JFXTextArea edtDatabaseUri;
+
+    @FXML
+    private JFXButton btnLogin;
 
 
     @Override
@@ -32,6 +40,22 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
+    }
+
+    public void connectToDatabase() {
+        // Handle database connecting here conchos
+        setAlert("Oops... Something wrong", "Can't connect to database");
+    }
+
+    private void setAlert(String heading, String message) {
+        JFXDialogLayout content= new JFXDialogLayout();
+        content.setHeading(new Text(heading));
+        content.setBody(new Text(message));
+        JFXDialog dialog =new JFXDialog(root, content, JFXDialog.DialogTransition.CENTER);
+        JFXButton button=new JFXButton("OK");
+        button.setOnAction(event -> dialog.close());
+        content.setActions(button);
+        dialog.show();
     }
 
     public static void main(String[] args) {
