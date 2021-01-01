3a257909fef6e6901a967da9d805ab72ef67e775
Nguyen Tuan Dat <53074785+itdat@users.noreply.github.com>
2021-01-01 16:19:02 +0700

    Add customized title bar's handlers

--- src/main/java/ui/main/Main.java
+++ src/main/java/ui/main/Main.java
@@ -1,14 +1,48 @@
 package  ui.main;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Main extends Application {
+
+    private double x, y;
+
+    public void handleDragged(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setX(event.getScreenX() - x);
+        stage.setY(event.getScreenY() - y);
+    }
+
+    public void handlePressed(MouseEvent event) {
+        x = event.getX();
+        y = event.getY();
+    }
+
+    public void handleReleased(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        if (stage.getY() < 0) {
+            stage.setY(0);
+        }
+    }
+
+    public void close(ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    public void minimize(ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
