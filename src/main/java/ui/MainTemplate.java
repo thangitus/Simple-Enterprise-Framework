0c12bc36285d64b769c55b610d30cdbd8663dc0b
Nguyen Tuan Dat <53074785+itdat@users.noreply.github.com>
2021-01-15 00:23:49 +0700

    Enhance UI

--- src/main/java/ui/MainTemplate.java
+++ src/main/java/ui/MainTemplate.java
@@ -2,18 +2,27 @@ package ui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class MainTemplate extends Application {
+
+public class MainTemplate extends Application implements Initializable {
+
+    List<Scene> scenes = new ArrayList<>();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/SceneTemplate.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/LoginSceneTemplate.fxml"));
         primaryStage.setTitle("CRUD Framework version 1.0");
         primaryStage.centerOnScreen();
         Scene scene = new Scene(root);
@@ -25,5 +34,22 @@ public class MainTemplate extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        for (int i = 0; i < 3; i++) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/SceneTemplate.fxml"));
+            Parent root = null;
+            try {
+                root = loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            assert root != null;
+            Scene scene = new Scene(root);
+            scene.setFill(Color.TRANSPARENT);
+            scenes.add(scene);
+        }
     }
 }
